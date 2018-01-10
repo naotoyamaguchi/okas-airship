23c1f52aa83277a78c3238bdf163bee51842d3fe
Nick Lee <nicklee808@gmail.com>
2018-01-10 12:39:32 -1000

    added css files for estimates and contact pages

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
-
+import './index.css';
 
 class Contact extends Component {
 
