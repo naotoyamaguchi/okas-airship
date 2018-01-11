89f9df38e18c79a54d78e68395a1e1d1fc7eb6d6
Nick Lee <nicklee808@gmail.com>
2018-01-10 15:20:52 -1000

    presentableish

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -26,19 +26,18 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact">
-          <div>
-            <div className="contact-header">
-              <h1 className="contact-title">
-                <Link to="/appointment">Make an Appointment!</Link>
-              </h1>
-            </div>
-            <hr></hr>
-            <div className="contact-body">
-              <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum alias dolore illo dolorum eum, enim perspiciatis doloribus est recusandae quo quibusdam consequatur hic, quos animi praesentium ipsum rem cumque. Maiores?</span>
-              <span>Atque adipisci pariatur magni, eligendi, eaque in asperiores eius laboriosam iure ipsa nostrum obcaecati autem placeat blanditiis dicta! Ab non harum rem dolor ipsum et necessitatibus! Repudiandae, dolores maiores explicabo!</span>
-              <span>Suscipit illo ratione blanditiis in odio cupiditate repudiandae maxime nostrum tempore eaque odit delectus, sunt nobis, ipsa beatae asperiores unde labore natus praesentium maiores vitae at exercitationem consequuntur deserunt! Omnis!</span></p>
-            </div>
-          </div>
+        <div className="contact-header">
+          <h1 className="contact-title">
+            {/*<Link to="/appointment">Make an Appointment!</Link>*/}
+            Contact Us
+          </h1>
+        </div>
+        <div className="hr"><hr/></div>
+        <div className="contact-body">
+          <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum alias dolore illo dolorum eum, enim perspiciatis doloribus est recusandae quo quibusdam consequatur hic, quos animi praesentium ipsum rem cumque. Maiores?</span>
+          <span>Atque adipisci pariatur magni, eligendi, eaque in asperiores eius laboriosam iure ipsa nostrum obcaecati autem placeat blanditiis dicta! Ab non harum rem dolor ipsum et necessitatibus! Repudiandae, dolores maiores explicabo!</span>
+          <span>Suscipit illo ratione blanditiis in odio cupiditate repudiandae maxime nostrum tempore eaque odit delectus, sunt nobis, ipsa beatae asperiores unde labore natus praesentium maiores vitae at exercitationem consequuntur deserunt! Omnis!</span></p>
+        </div>
       </div>
     );
   }
