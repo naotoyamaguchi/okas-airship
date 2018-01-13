b1e2abf97245b2dcd32b52182d985a9b39a933a9
Nick Lee <nicklee808@gmail.com>
2018-01-12 17:13:38 -1000

    updated form fields

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -38,16 +38,18 @@ class Contact extends Component {
             <div className="contact-form">
               <h3 className="body-subheader">Send Us a Message</h3>
               Name:<br/>
-              <input type="text" name="name"/><br/>
+              <input type="text" name="name" size="45"/><br/><br/>
               Email:<br/>
-              <input type="text" name="email"/><br/>
+              <input type="text" name="email"size="45"/><br/><br/>
               Phone:<br/>
-              <input type="text" name="phone"/><br/>
+              <input type="text" name="phone"size="45"/><br/><br/>
               What is your preferred method of contact?<br/>
-              <input type="radio" name="contact-pref"/> Email<br/>
-              <input type="radio" name="contact-pref"/> Phone<br/>
+                <select>
+                <option value="email">Email</option>
+                <option value="phone">Phone</option>
+                </select><br/><br/>
               Message:<br/>
-              <input type="text" name="message"/><br/>
+              <input type="text" name="message"/><br/><br/>
               <button className="submit">Submit ›</button>
             </div>
             <div className="oka-contact-info">
