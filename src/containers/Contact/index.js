2dd4b9f68a01ec77ef98836ba98c9275b589d5fd
Naoto Yamaguchi <naotoyamaguchi@users.noreply.github.com>
2018-01-12 16:48:32 -1000

    Max width on page so zooming out / high resolution does not ruin scale

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -25,50 +25,52 @@ class Contact extends Component {
 
   render() {
     return (
-      <div className="contact">
-        <div className="contact-header">
-          <h1 className="contact-title">
-            {/*<Link to="/appointment">Make an Appointment!</Link>*/}
-            Contact Us
-          </h1>
-        </div>
-        <hr/>
-        <div className="contact-body">
-          <div className="contact-form">
-            <h3 className="body-subheader">Send Us a Message</h3>
-            Name:<br/>
-            <input type="text" name="name"/><br/>
-            Email:<br/>
-            <input type="text" name="email"/><br/>
-            Phone:<br/>
-            <input type="text" name="phone"/><br/>
-            What is your preferred method of contact?<br/>
-            <input type="radio" name="contact-pref"/> Email<br/>
-            <input type="radio" name="contact-pref"/> Phone<br/>
-            Message:<br/>
-            <input type="text" name="message"/><br/>
-            <button className="submit">Submit ›</button>
+      <div className="App">
+        <div className="contact">
+          <div className="contact-header">
+            <h1 className="contact-title">
+              {/*<Link to="/appointment">Make an Appointment!</Link>*/}
+              Contact Us
+            </h1>
           </div>
-          <div className="oka-contact-info">
-            <h3 className="body-subheader">Contact Info</h3>
-            Address:<br/>
-            94-173 Leokane St.<br/>
-            Waipahu, HI 96897<br/><br/>
-            Phone:<br/>
-            808-677-5474<br/><br/>
-            Hours:<br/>
-            7:30 – 4:30 M–F
+          <hr/>
+          <div className="contact-body">
+            <div className="contact-form">
+              <h3 className="body-subheader">Send Us a Message</h3>
+              Name:<br/>
+              <input type="text" name="name"/><br/>
+              Email:<br/>
+              <input type="text" name="email"/><br/>
+              Phone:<br/>
+              <input type="text" name="phone"/><br/>
+              What is your preferred method of contact?<br/>
+              <input type="radio" name="contact-pref"/> Email<br/>
+              <input type="radio" name="contact-pref"/> Phone<br/>
+              Message:<br/>
+              <input type="text" name="message"/><br/>
+              <button className="submit">Submit ›</button>
+            </div>
+            <div className="oka-contact-info">
+              <h3 className="body-subheader">Contact Info</h3>
+              Address:<br/>
+              94-173 Leokane St.<br/>
+              Waipahu, HI 96897<br/><br/>
+              Phone:<br/>
+              808-677-5474<br/><br/>
+              Hours:<br/>
+              7:30 – 4:30 M–F
+            </div>
           </div>
+          <hr/>
+          <div className="directions">
+            <h3 className="body-subheader">Directions</h3>
+            <div className="directions-subheader">From the East:</div>
+            Head west<br/><br/>
+            <div className="directions-subheader">From the West:</div>
+            Head east
+          </div>
+          <div className="bumper"/>
         </div>
-        <hr/>
-        <div className="directions">
-          <h3 className="body-subheader">Directions</h3>
-          <div className="directions-subheader">From the East:</div>
-          Head west<br/><br/>
-          <div className="directions-subheader">From the West:</div>
-          Head east
-        </div>
-        <div className="bumper"/>
       </div>
     );
   }
--- src/containers/Estimates/index.js
+++ src/containers/Estimates/index.js
@@ -25,25 +25,27 @@ class Estimates extends Component {
 
   render() {
     return (
-      <div className="estimates">
-        <div className="estimates-header">
-          <h1 className="estimates-title">
-            Estimates
-          </h1>
+      <div className="App">
+        <div className="estimates">
+          <div className="estimates-header">
+            <h1 className="estimates-title">
+              Estimates
+            </h1>
+          </div>
+          <div className="hr"><hr/></div>
+          <div className="estimates-body">
+            <h1>2017 Community</h1>
+            <p>Oka's in the community this year Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla eveniet placeat esse! Dolorem nemo tempora eos maxime explicabo assumenda voluptas necessitatibus omnis, odit, inventore dignissimos veniam perferendis incidunt, alias quidem!</p>
+            <h1>2016 Community</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero vitae, maiores beatae. Est voluptates cum adipisci magni vel vitae totam quas odio ut, commodi, quia illo eligendi quibusdam, optio. Unde.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis neque explicabo vitae quisquam corporis fugit nam, velit ex, placeat minima porro saepe impedit commodi magnam! Unde amet at totam sequi.</p>
+            <h1>2015 Community</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum quisquam beatae eos. Porro explicabo asperiores deleniti placeat illo similique itaque, reiciendis dolores illum perferendis maiores mollitia eaque ut est labore.</p>
+            <p>Oka's in the community this year Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla eveniet placeat esse! Dolorem nemo tempora eos maxime explicabo assumenda voluptas necessitatibus omnis, odit, inventore dignissimos veniam perferendis incidunt, alias quidem!</p>
+            <p>Oka's in the community this year Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla eveniet placeat esse! Dolorem nemo tempora eos maxime explicabo assumenda voluptas necessitatibus omnis, odit, inventore dignissimos veniam perferendis incidunt, alias quidem!</p>
+          </div>
         </div>
-        <div className="hr"><hr/></div>
-        <div className="estimates-body">
-          <h1>2017 Community</h1>
-          <p>Oka's in the community this year Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla eveniet placeat esse! Dolorem nemo tempora eos maxime explicabo assumenda voluptas necessitatibus omnis, odit, inventore dignissimos veniam perferendis incidunt, alias quidem!</p>
-          <h1>2016 Community</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero vitae, maiores beatae. Est voluptates cum adipisci magni vel vitae totam quas odio ut, commodi, quia illo eligendi quibusdam, optio. Unde.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis neque explicabo vitae quisquam corporis fugit nam, velit ex, placeat minima porro saepe impedit commodi magnam! Unde amet at totam sequi.</p>
-          <h1>2015 Community</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum quisquam beatae eos. Porro explicabo asperiores deleniti placeat illo similique itaque, reiciendis dolores illum perferendis maiores mollitia eaque ut est labore.</p>
-          <p>Oka's in the community this year Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla eveniet placeat esse! Dolorem nemo tempora eos maxime explicabo assumenda voluptas necessitatibus omnis, odit, inventore dignissimos veniam perferendis incidunt, alias quidem!</p>
-          <p>Oka's in the community this year Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla eveniet placeat esse! Dolorem nemo tempora eos maxime explicabo assumenda voluptas necessitatibus omnis, odit, inventore dignissimos veniam perferendis incidunt, alias quidem!</p>
-        </div>
-      </div>        
+      </div>     
     );
   }
 }
