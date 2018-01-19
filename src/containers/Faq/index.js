e9b23743f8ca0f653bc78791a791295815839b8a
Naoto Yamaguchi <naotoyamaguchi@users.noreply.github.com>
2018-01-18 16:51:12 -1000

    Made dynamic accordian accordingly to Airship API

--- src/containers/Faq/index.js
+++ src/containers/Faq/index.js
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Faq extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      faqs: []
+    };
+  }
 
   componentDidMount() {
     var elem = ReactDOM.findDOMNode(this);
@@ -12,6 +19,19 @@ class Faq extends Component {
       elem.style.transition = "1s ease";
       elem.style.opacity = 1;
     });
+
+
+    fetch("https://okas.airshipcms.io/api/aerostat_collection/faq")
+      .then(res => res.json())
+      .then(faqs => {
+        faqs.map(item =>
+          item.fields.map(
+            field => (item[field.variable_name] = field.value)
+            )
+          );
+        this.setState({faqs});
+        // this.setState({  });
+      });
   }
 
   componentWillUnmount(){
@@ -23,67 +43,38 @@ class Faq extends Component {
     });
   }
 
+
+
   render() {
+    console.log(this.state);
+    var accordianCard = this.state.faqs.map(function(faq, index){
+                      return <div className="card" key={index}>
+                        <div className="card-header" role="tab" id="headingOne">
+                          <h5 className="mb-0">
+                            <a data-toggle="collapse" href={"#"+faq.id} role="button" aria-expanded="true" aria-controls={faq.id}>
+                              {faq.question}
+                            </a>
+                          </h5>
+                        </div>
+
+                        <div id={faq.id} className="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+                          <div className="card-body">
+                            {faq.answer}
+                          </div>
+                        </div>
+                      </div>;
+                    })
     return (
       <div className="App">
         <div className="faq">
-          {/*<a className="faq-question-link" href="#question-10">Question 10</a>*/}
           <div className="faq-header">
             <h1 className="faq-title">Frequently Asked Questions
             </h1>
           </div>
           <hr></hr>
-
-
           <div id="accordion" role="tablist">
-            <div className="card">
-              <div className="card-header" role="tab" id="headingOne">
-                <h5 className="mb-0">
-                  <a data-toggle="collapse" href="#collapseOne" role="button" aria-expanded="true" aria-controls="collapseOne">
-                    Collapsible Group Item #1
-                  </a>
-                </h5>
-              </div>
-
-              <div id="collapseOne" className="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
-                <div className="card-body">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                </div>
-              </div>
-            </div>
-            <div className="card">
-              <div className="card-header" role="tab" id="headingTwo">
-                <h5 className="mb-0">
-                  <a className="collapsed" data-toggle="collapse" href="#collapseTwo" role="button" aria-expanded="false" aria-controls="collapseTwo">
-                    Collapsible Group Item #2
-                  </a>
-                </h5>
-              </div>
-
-              <div id="collapseTwo" className="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
-                <div className="card-body">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                </div>
-              </div>
-            </div>
-            <div className="card">
-              <div className="card-header" role="tab" id="headingThree">
-                <h5 className="mb-0">
-                  <a className="collapsed" data-toggle="collapse" href="#collapseThree" role="button" aria-expanded="false" aria-controls="collapseThree">
-                    Collapsible Group Item #3
-                  </a>
-                </h5>
-              </div>
-
-              <div id="collapseThree" className="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
-                <div className="card-body">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                </div>
-              </div>
-            </div>
+            {accordianCard}
           </div>
-          
-          
         </div>        
       </div>
     );
