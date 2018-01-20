e7dbd8501c131672921fbadb34eabaf3635d4ee3
Nick Lee <nicklee808@gmail.com>
2018-01-19 16:33:28 -1000

    changed header colours

--- src/containers/Estimates/index.js
+++ src/containers/Estimates/index.js
@@ -34,7 +34,7 @@ class Estimates extends Component {
           </div>
           <div className="hr"><hr/></div>
           <div className="estimates-body">
-            <h1>Need a Quote?</h1>
+            <h1 className="subheader">Need a Quote?</h1>
             <p>If you're unsure of where to start, contact us for an appointment and we can arrange a free quote for whatever repair you need! Either message us for questions, or set up an appointment using our appointment system <Link to="/appointment">here</Link>. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor, elit at sagittis laoreet, urna risus dapibus nisl, et vehicula mauris dolor non felis. Phasellus quam lectus, auctor at nisi id, aliquam convallis mauris. Donec quis mauris nisl. Mauris a tempus eros. Sed vel maximus purus. Praesent tempus odio a diam posuere, nec ultricies elit mattis. Nullam vitae leo sed quam finibus mattis ac mattis nisl. Donec sit amet magna libero. Sed non semper ipsum, at porta nisl. Aenean rutrum et nibh nec condimentum. Nam ligula nibh, mattis in nulla luctus, cursus pellentesque magna. In a est quis purus euismod egestas nec eget dui.</p>
           </div>
         </div>
