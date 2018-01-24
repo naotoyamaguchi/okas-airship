15928f3e9da9406748de7741ddedf4ccaac95eb6
Nick Lee <nicklee808@gmail.com>
2018-01-23 16:35:53 -1000

    added more sample photos to carousel

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -47,10 +47,13 @@ class Home extends Component {
               <img className="d-block w-100" src={require("../../assets/employee-photo3.jpeg")} alt="First slide"/>
             </div>
             <div className="carousel-item">
-              <img className="d-block w-100" src={require("../../assets/stock-carousel-photo.jpeg")} alt="Second slide"/>
+              <img className="d-block w-100" src={require("../../assets/stock-carousel-photo2.jpg")} alt="Second slide"/>
             </div>
             <div className="carousel-item">
-              <img className="d-block w-100" src={require("../../assets/stock-carousel-photo.jpeg")} alt="Third slide"/>
+              <img className="d-block w-100" src={require("../../assets/stock-carousel-photo3.jpg")} alt="Third slide"/>
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src={require("../../assets/stock-carousel-photo.jpeg")} alt="Second slide"/>
             </div>
           </div>
           <a className="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
