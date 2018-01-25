861c712c5f1197f4f6e87540193e3dcf33d2d593
Naoto Yamaguchi <naotoyamaguchi@users.noreply.github.com>
2018-01-24 17:35:53 -1000

    fixed estimates header padding

--- src/containers/Estimates/index.js
+++ src/containers/Estimates/index.js
@@ -31,8 +31,9 @@ class Estimates extends Component {
             <h1 className="header-title">
               Estimates
             </h1>
+            <hr/>
           </div>
-          <div className="hr"><hr/></div>
+          
           <div className="estimates-body">
             <h1 className="subheader">Need a Quote?</h1>
             <p>If you're unsure of where to start, contact us for an appointment and we can arrange a free quote for whatever repair you need! Either message us for questions, or set up an appointment using our appointment system <Link to="/appointment">here</Link>.</p>
