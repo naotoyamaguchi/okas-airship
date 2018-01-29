0c91c7af30e9740bb125638c183fa62ff1cfbd79
Nick Lee <nicklee808@gmail.com>
2018-01-29 13:42:14 -1000

    base setup for confirmation and insurance

---
+++ src/containers/ContactConfirmation/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom'; 
+import './index.css';
+
+class ContactConfirmation extends Component {
+
+  render () {
+    return (
+      <div className="App">
+        <div className="confirmation">
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default ContactConfirmation;
---
+++ src/containers/Insurance/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom'; 
+import './index.css';
+
+class Insurance extends Component {
+
+  render () {
+    return (
+      <div className="App">
+        <div className="confirmation">
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Insurance;
