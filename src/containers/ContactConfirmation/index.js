ad13b9b7d6b71d3847787d2871313fd5a4b9e7cb
Nick Lee <nicklee808@gmail.com>
2018-01-29 18:17:58 -1000

    insurance base layout

--- src/containers/ContactConfirmation/index.js
+++ src/containers/ContactConfirmation/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
-
 import './index.css';
 
 class ContactConfirmation extends Component {
@@ -22,7 +21,6 @@ class ContactConfirmation extends Component {
       </div>
     );
   }
-
 }
 
 export default ContactConfirmation;
