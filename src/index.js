a0bfefef9a30813e4a1399595c9f020f3478ed7d
Naoto Yamaguchi <naotoyamaguchi@users.noreply.github.com>
2018-01-29 19:27:48 -1000

    all invalid routes go to homepage

--- src/index.js
+++ src/index.js
@@ -11,19 +11,22 @@ import ContactConfirmation from './containers/ContactConfirmation';
 import Insurance from './containers/Insurance';
 import "./index.css";
 import "./borders.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/contact" component={Contact} />
-      <Route exact path="/faq" component={Faq} />
-      <Route exact path="/estimates" component={Estimates} />
-      <Route exact path="/services" component={Services} />
-      <Route exact path="/appointment" component={Appointment} />
-      <Route exact path="/contactconfirmation" component={ContactConfirmation} />
-      <Route exact path="/insurance" component={Insurance} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/faq" component={Faq} />
+        <Route exact path="/estimates" component={Estimates} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/appointment" component={Appointment} />
+        <Route exact path="/contactconfirmation" component={ContactConfirmation} />
+        <Route exact path="/insurance" component={Insurance} />
+        <Route component={Home} />
+      </Switch>
     </App>
     
   </BrowserRouter>,
