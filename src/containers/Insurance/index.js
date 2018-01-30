2965e68beb28f04626121241efc55178b8b84e47
Nick Lee <nicklee808@gmail.com>
2018-01-29 18:25:19 -1000

    contact confirmation and insurance page now have same base layouts

--- src/containers/Insurance/index.js
+++ src/containers/Insurance/index.js
@@ -8,13 +8,13 @@ class Insurance extends Component {
   render () {
     return (
       <div className="App">
-        <div className="insurance">
-          <div className="insurance-header">
+        <div className="insurance-page">
+          <div className="insurance-page-header">
             <h1 className="header-title">
               Insurance Title Here
             </h1>
           </div>
-          <div className="insurance-body">
+          <div className="insurance-page-body">
             Click <Link to="/" className="return-home">HERE</Link> to return from whence you came
           </div>
         </div>
